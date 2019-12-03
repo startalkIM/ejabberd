ebaef2e04ad511d2fcf147bdc46ff49b0bac8088
boyofmonk@gmail.com <boyofmonk@gmail.com>
2019-12-02 22:52:01 -0500

    添加禁止陌生人单聊的代码

--- src/mod_black_check.erl
+++ src/mod_black_check.erl
@@ -28,7 +28,14 @@ check_packet(_, _User, _Server, _UserList,
     FromStr = jid:to_string(jid:replace_resource(To, <<"">>)),
     ToStr = jid:to_string(jid:replace_resource(From, <<"">>)),
     Type = fxml:get_attr_s(<<"type">>, Attrs),
-    do_check_packet(FromStr, ToStr, Type);
+    case do_check_packet(FromStr, ToStr, Type) of
+        deny -> deny;
+        R ->
+            case ejabberd_config:get_option(message_permissions, fun(Url)-> Url end, undefined) of
+                true -> do_check_priv(From, To);
+                _ -> R
+            end
+    end;
 check_packet(_, _User, _Server, _UserList, _, _) ->
     allow.
 
@@ -40,6 +47,12 @@ do_check_packet(FromStr, ToStr, _) ->
     case mod_redis:hash_get(11, <<"blacklist:", FromStr/binary>>, ToStr) of
         {ok, undefined} -> allow;
         _ -> deny 
+    end.
+
+do_check_priv(From, To) ->
+    case catch  ejabberd_sql:sql_query([<<"select relationship from user_friends where username='">>, From#jid.luser, <<"' and userhost='">>, From#jid.lserver, <<"' and friend ='">>, To#jid.luser, <<"' and host='">>, To#jid.lserver, <<"';">>]) of
+        {select, _, [[<<"1">>]]} -> allow;
+        _ -> deny
     end.
 
 depends(_Host, _Opts) ->
